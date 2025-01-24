f8b3dae93554cbff4cc6e5b3eebb7820afa463e3
Zaden Ngobeni <zangobejhb024@student.wethinkcode.co.za>
2025-01-24 21:46:45 +0200

    abstraction

--- fish.py
+++ fish.py
@@ -1,8 +1,10 @@
-
+from abc import ABC, abstractmethod
 class Fish:
 
+    @abstractmethod
     def personality():
         pass
     
+    @abstractmethod
     def physical_appearance():
         pass
