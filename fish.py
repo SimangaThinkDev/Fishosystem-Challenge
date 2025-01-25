82969a13ca15db17bf1fc0059253c0cc2eb3dd64
Zaden Ngobeni <zangobejhb024@student.wethinkcode.co.za>
2025-01-25 11:29:34 +0200

    personality

--- fish.py
+++ fish.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+from random import *
 class Fish:
 
     @abstractmethod
@@ -8,3 +9,13 @@ class Fish:
     @abstractmethod
     def physical_appearance():
         pass
+
+class Parent(Fish):
+
+    def __init__(self):
+        self.possibleGenders = ['male', 'female']
+        self.gender = choice(self.possibleGenders)
+    
+    def personality():
+        possible_personalities = ["confident", "shy", "nosy", "dual-nature"]
+        fish_personality = []
