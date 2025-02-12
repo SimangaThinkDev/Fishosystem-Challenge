d11861845235db65477a26c305b4aba52f25ca16
Simangaliso Phakwe <siphakwjhb024@student.wethinkcode.co.za>
2025-02-12 20:44:19 +0200

    cleaner code

--- fisho.py
+++ fisho.py
@@ -1,16 +1,6 @@
 from clear import clear
 import random, time
 
-# try:
-#     while True:
-#         grounds = [[random.choices([" ", ">", "#"], [25, 2, 1])[0] for i in range(20)] for j in range(20)]
-#         clear()
-#         for row in grounds:
-#             print(row)
-#         time.sleep(1)
-# except KeyboardInterrupt:
-#     print("Thank you!")
-
 fishosys = [[random.choices([" ", ">", "#"], [30, 2, 0.5])[0] for i in range(20)] for j in range(20)]
 
 def fish_noticer(fishosys = fishosys):
